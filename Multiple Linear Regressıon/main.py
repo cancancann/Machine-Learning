cb8837af56581d37f5826cf5c7c9147cbe797bae
cancancann <73189111+cancancann@users.noreply.github.com>
2022-05-15 14:26:39 +0300

    Multiple Linear Regression

--- Multiple Linear Regressıon/main.py
+++ Multiple Linear Regressıon/main.py
@@ -1,1 +1,23 @@
+from operator import mul
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+## y = b0 + b1 * x1 +b2*x2...(feature sayısı artıyor..) algorithm
+
+df = pd.read_csv("multiple_linear_regression_dataset.csv",sep=";")
+
+# print(df)
+
+x = df.iloc[:,[0,2]].values 
+y = df.maas.values.reshape(-1,1)
+
+#train fit
+multiple_linear_reg = LinearRegression()
+multiple_linear_reg.fit(x,y)
+
+#predict
+result = multiple_linear_reg.predict(np.array([[10,35],[5,35]]))
+# print(result)
+
